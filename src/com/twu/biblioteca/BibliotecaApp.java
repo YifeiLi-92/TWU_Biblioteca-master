03197df4a5c9711d48db1ec2ba2aa591b9db9a21
Yifei Li <yfli@CNyfli.local>
2017-08-05 11:55:35 +0800

    fourth commit.finish release 2 main function

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -6,16 +6,22 @@ public class BibliotecaApp {
     public static final int LIST_BOOKS = 1;
     public static final int CHECK_OUT_BOOK = 2;
     public static final int RETURN_BOOK = 3;
-    public static final int QUIT = 4;
+    public static final int LIST_MOVIES = 4;
+    public static final int CHECK_OUT_MOVIES = 5;
+    public static final int QUIT = 6;
 
     public static void main(String[] args) {
-
-        /*Books books = new Books();
+        Books books = new Books();
         books.addBook(new Book("1","2","3"));
         books.addBook(new Book("4","5","6"));
         Books borrowBooks = new Books();
 
-        User user = new User(books,borrowBooks);
+        Movies movies = new Movies();
+        movies.addMovie(new Movie("1","2","3","4"));
+        movies.addMovie(new Movie("5","6","7","8"));
+        Movies borrowMovies = new Movies();
+
+        User user = new User("lyf","123456",books,borrowBooks,movies,borrowMovies);
 
 
         System.out.println("***************   Welcome to Biblioteca!   ***************");
@@ -25,7 +31,8 @@ public class BibliotecaApp {
         int i = 0;
         while (true) {
             try {
-                System.out.println("\n********************   Main Menu   ********************\nList Books(1)   Check-out Book(2)   Return book(3)   Quit(4)");
+                System.out.println("\n********************   Main Menu   ********************\nList Books(1)   Check-out Book(2)   Return book(3)   " +
+                        "\nList Movies(4)     Check-out Movies(5)     Quit(6)");
 
                 int select = Integer.valueOf(input.next());
                 switch (select) {
@@ -34,17 +41,44 @@ public class BibliotecaApp {
                         System.out.printf(user.getAllBooks().showList());
                         break;
                     case CHECK_OUT_BOOK:
+                        System.out.printf("Login\nName:");
+                        String userN = input.next();
+                        System.out.printf("Password:");
+                        String pwd = input.next();
+                        if(!user.login(userN,pwd)) {
+                            System.out.printf("Wrong Account!\n");
+                            break;
+                        }
+
                         System.out.printf("Input the name of the book:\n");
                         String name = input.next();
 
                         System.out.printf(user.checkOutBook(name));
                         break;
                     case RETURN_BOOK:
+                        System.out.printf("Login\nName:");
+                        userN = input.next();
+                        System.out.printf("Password:");
+                        pwd = input.next();
+                        if(!user.login(userN,pwd)) {
+                            System.out.printf("Wrong Account!\n");
+                            break;
+                        }
+
                         System.out.printf("Input the name of the book:\n");
                         name = input.next();
 
                         System.out.printf(user.returnBook(name));
 
+                        break;
+                    case LIST_MOVIES:
+                        System.out.printf("Name     Year     Director     Movie Rating\n");
+                        System.out.printf(user.getAllMovies().showList());
+                        break;
+                    case CHECK_OUT_MOVIES:
+                        System.out.printf("Input the name of the movie:\n");
+                        name = input.next();
+                        System.out.printf(user.checkOutMovie(name));
                         break;
                     case QUIT:
                         flag = 1;
@@ -58,6 +92,6 @@ public class BibliotecaApp {
             }
             if (flag == 1)
                 break;
-        }*/
+        }
     }
 }
